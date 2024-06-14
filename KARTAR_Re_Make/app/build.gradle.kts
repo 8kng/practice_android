80670522547dc02e4a924ff57620fe88bfc31804
8kng <halkgonke278@gmail.com>
2024-06-14 21:02:57 +0900

    [feat]ktlintの設定をしたかったが失敗

--- KARTAR_Re_Make/app/build.gradle.kts
+++ KARTAR_Re_Make/app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    ktlint("com.pinterest:ktlint:0.43.1")
 }
